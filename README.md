82e43f4377a5c9e4310b8182dfd942755c69918d
Unknown <alexjhon@live.com>
2019-03-05 12:58:27 -0500

    Update .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-FSM-in-vhdl
+
+## **Diseño de un controlador de máquina expendedora**
+
+<p align="center"> 
+<img src="https://i.postimg.cc/Sxn9GcdK/vmachine.png" alt="drawing"/>
+</p>
+
+La máquina expendedora tiene las siguientes características (descripción textual): 
+
+La máquina expendedora vende solo un tipo de producto, y cada producto cuesta $ 0.25;
+- Sólo se aceptan $ 0.25 monedas;
+- Una moneda válida de $ 0.25 pesa 5.670 g;
+- Las monedas no válidas se rechazan automáticamente, y el circuito de control no necesita hacer nada al respecto;
+- Hay una ranura para insertar $ 0.25 monedas en la máquina expendedora;
+- Hay un circuito de detección de monedas que emite "1" cada vez que se inserta una moneda de $ 0.25 en la ranura, o "0" de lo contrario
+- Hay una balanza digital que genera "1" cuando se detecta un peso de 5.670 g (o más);
+- Hay un ‘botón de entrega’ ’que emite‘ 1 ’cada vez que se presiona;
+- Hay un ‘botón de retorno de moneda’ ’que emite‘ 1 ’cada vez que se presiona;
+- Hay una ‘taza de devolución de monedas’ para recoger las monedas expulsadas por el ‘botón de devolución de monedas’ (y también las monedas rechazadas);
+- Hay una puerta donde el usuario puede recoger su producto, después de haber presionado el "botón de entrega";
+- Cuando la máquina expendedora se queda sin productos, se cierra y el circuito de control no necesita hacer nada al respecto.
---
+++ VendingMachine/isim/Test_MaquinaExpendedora_isim_beh.exe.sim/work/a_0478438494_3212880686.c
@@ -0,0 +1,419 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Users/Dell/Documents/ProyectosVHDL/2019/VendingMachineTRESPROCESO/VMachine/MaquinaExpendedora.vhd";
+extern char *IEEE_P_2592010699;
+
+unsigned char ieee_p_2592010699_sub_1258338084_503743352(char *, char *, unsigned int , unsigned int );
+unsigned char ieee_p_2592010699_sub_1744673427_503743352(char *, char *, unsigned int , unsigned int );
+
+
+static void work_a_0478438494_3212880686_p_0(char *t0)
+{
+    char *t1;
+    unsigned char t2;
+    char *t3;
+    char *t4;
+    unsigned char t5;
+    unsigned char t6;
+    char *t7;
+    char *t8;
+    char *t9;
+    char *t10;
+
+LAB0:    xsi_set_current_line(22, ng0);
+    t1 = (t0 + 1632U);
+    t2 = ieee_p_2592010699_sub_1258338084_503743352(IEEE_P_2592010699, t1, 0U, 0U);
+    if (t2 != 0)
+        goto LAB2;
+
+LAB4:
+LAB3:    t1 = (t0 + 4448);
+    *((int *)t1) = 1;
+
+LAB1:    return;
+LAB2:    xsi_set_current_line(23, ng0);
+    t3 = (t0 + 1832U);
+    t4 = *((char **)t3);
+    t5 = *((unsigned char *)t4);
+    t6 = (t5 == (unsigned char)2);
+    if (t6 != 0)
+        goto LAB5;
+
+LAB7:    xsi_set_current_line(26, ng0);
+    t1 = (t0 + 2472U);
+    t3 = *((char **)t1);
+    t2 = *((unsigned char *)t3);
+    t1 = (t0 + 4560);
+    t4 = (t1 + 56U);
+    t7 = *((char **)t4);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    *((unsigned char *)t9) = t2;
+    xsi_driver_first_trans_fast(t1);
+
+LAB6:    goto LAB3;
+
+LAB5:    xsi_set_current_line(24, ng0);
+    t3 = (t0 + 4560);
+    t7 = (t3 + 56U);
+    t8 = *((char **)t7);
+    t9 = (t8 + 56U);
+    t10 = *((char **)t9);
+    *((unsigned char *)t10) = (unsigned char)0;
+    xsi_driver_first_trans_fast(t3);
+    goto LAB6;
+
+}
+
+static void work_a_0478438494_3212880686_p_1(char *t0)
+{
+    char *t1;
+    char *t2;
+    unsigned char t3;
+    char *t4;
+    char *t5;
+    unsigned char t6;
+    unsigned char t7;
+    char *t8;
+    char *t9;
+    char *t10;
+    char *t11;
+    static char *nl0[] = {&&LAB3, &&LAB4, &&LAB5, &&LAB6, &&LAB7};
+
+LAB0:    xsi_set_current_line(37, ng0);
+    t1 = (t0 + 2312U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t1 = (char *)((nl0) + t3);
+    goto **((char **)t1);
+
+LAB2:    t1 = (t0 + 4464);
+    *((int *)t1) = 1;
+
+LAB1:    return;
+LAB3:    xsi_set_current_line(39, ng0);
+    t4 = (t0 + 1512U);
+    t5 = *((char **)t4);
+    t6 = *((unsigned char *)t5);
+    t7 = (t6 == (unsigned char)3);
+    if (t7 != 0)
+        goto LAB8;
+
+LAB10:    t1 = (t0 + 1192U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t6 = (t3 == (unsigned char)3);
+    if (t6 != 0)
+        goto LAB11;
+
+LAB12:    t1 = (t0 + 1032U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t6 = (t3 == (unsigned char)3);
+    if (t6 != 0)
+        goto LAB13;
+
+LAB14:    xsi_set_current_line(46, ng0);
+    t1 = (t0 + 4624);
+    t2 = (t1 + 56U);
+    t4 = *((char **)t2);
+    t5 = (t4 + 56U);
+    t8 = *((char **)t5);
+    *((unsigned char *)t8) = (unsigned char)0;
+    xsi_driver_first_trans_fast(t1);
+
+LAB9:    goto LAB2;
+
+LAB4:    xsi_set_current_line(50, ng0);
+    t1 = (t0 + 4624);
+    t2 = (t1 + 56U);
+    t4 = *((char **)t2);
+    t5 = (t4 + 56U);
+    t8 = *((char **)t5);
+    *((unsigned char *)t8) = (unsigned char)0;
+    xsi_driver_first_trans_fast(t1);
+    goto LAB2;
+
+LAB5:    xsi_set_current_line(53, ng0);
+    t1 = (t0 + 1352U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t6 = (t3 == (unsigned char)2);
+    if (t6 != 0)
+        goto LAB15;
+
+LAB17:    xsi_set_current_line(56, ng0);
+    t1 = (t0 + 4624);
+    t2 = (t1 + 56U);
+    t4 = *((char **)t2);
+    t5 = (t4 + 56U);
+    t8 = *((char **)t5);
+    *((unsigned char *)t8) = (unsigned char)4;
+    xsi_driver_first_trans_fast(t1);
+
+LAB16:    goto LAB2;
+
+LAB6:    xsi_set_current_line(60, ng0);
+    t1 = (t0 + 1352U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t6 = (t3 == (unsigned char)2);
+    if (t6 != 0)
+        goto LAB18;
+
+LAB20:    xsi_set_current_line(63, ng0);
+    t1 = (t0 + 4624);
+    t2 = (t1 + 56U);
+    t4 = *((char **)t2);
+    t5 = (t4 + 56U);
+    t8 = *((char **)t5);
+    *((unsigned char *)t8) = (unsigned char)1;
+    xsi_driver_first_trans_fast(t1);
+
+LAB19:    goto LAB2;
+
+LAB7:    xsi_set_current_line(67, ng0);
+    t1 = (t0 + 4624);
+    t2 = (t1 + 56U);
+    t4 = *((char **)t2);
+    t5 = (t4 + 56U);
+    t8 = *((char **)t5);
+    *((unsigned char *)t8) = (unsigned char)0;
+    xsi_driver_first_trans_fast(t1);
+    goto LAB2;
+
+LAB8:    xsi_set_current_line(40, ng0);
+    t4 = (t0 + 4624);
+    t8 = (t4 + 56U);
+    t9 = *((char **)t8);
+    t10 = (t9 + 56U);
+    t11 = *((char **)t10);
+    *((unsigned char *)t11) = (unsigned char)1;
+    xsi_driver_first_trans_fast(t4);
+    goto LAB9;
+
+LAB11:    xsi_set_current_line(42, ng0);
+    t1 = (t0 + 4624);
+    t4 = (t1 + 56U);
+    t5 = *((char **)t4);
+    t8 = (t5 + 56U);
+    t9 = *((char **)t8);
+    *((unsigned char *)t9) = (unsigned char)2;
+    xsi_driver_first_trans_fast(t1);
+    goto LAB9;
+
+LAB13:    xsi_set_current_line(44, ng0);
+    t1 = (t0 + 4624);
+    t4 = (t1 + 56U);
+    t5 = *((char **)t4);
+    t8 = (t5 + 56U);
+    t9 = *((char **)t8);
+    *((unsigned char *)t9) = (unsigned char)3;
+    xsi_driver_first_trans_fast(t1);
+    goto LAB9;
+
+LAB15:    xsi_set_current_line(54, ng0);
+    t1 = (t0 + 4624);
+    t4 = (t1 + 56U);
+    t5 = *((char **)t4);
+    t8 = (t5 + 56U);
+    t9 = *((char **)t8);
+    *((unsigned char *)t9) = (unsigned char)0;
+    xsi_driver_first_trans_fast(t1);
+    goto LAB16;
+
+LAB18:    xsi_set_current_line(61, ng0);
+    t1 = (t0 + 4624);
+    t4 = (t1 + 56U);
+    t5 = *((char **)t4);
+    t8 = (t5 + 56U);
+    t9 = *((char **)t8);
+    *((unsigned char *)t9) = (unsigned char)0;
+    xsi_driver_first_trans_fast(t1);
+    goto LAB19;
+
+}
+
+static void work_a_0478438494_3212880686_p_2(char *t0)
+{
+    char *t1;
+    unsigned char t2;
+    char *t3;
+    char *t4;
+    unsigned char t5;
+    unsigned char t6;
+    char *t7;
+    char *t8;
+    char *t9;
+    char *t10;
+
+LAB0:    xsi_set_current_line(115, ng0);
+    t1 = (t0 + 1632U);
+    t2 = ieee_p_2592010699_sub_1744673427_503743352(IEEE_P_2592010699, t1, 0U, 0U);
+    if (t2 != 0)
+        goto LAB2;
+
+LAB4:
+LAB3:    t1 = (t0 + 4480);
+    *((int *)t1) = 1;
+
+LAB1:    return;
+LAB2:    xsi_set_current_line(117, ng0);
+    t3 = (t0 + 2312U);
+    t4 = *((char **)t3);
+    t5 = *((unsigned char *)t4);
+    t6 = (t5 == (unsigned char)0);
+    if (t6 != 0)
+        goto LAB5;
+
+LAB7:    t1 = (t0 + 2312U);
+    t3 = *((char **)t1);
+    t2 = *((unsigned char *)t3);
+    t5 = (t2 == (unsigned char)1);
+    if (t5 != 0)
+        goto LAB8;
+
+LAB9:    t1 = (t0 + 2312U);
+    t3 = *((char **)t1);
+    t2 = *((unsigned char *)t3);
+    t5 = (t2 == (unsigned char)2);
+    if (t5 != 0)
+        goto LAB10;
+
+LAB11:    t1 = (t0 + 2312U);
+    t3 = *((char **)t1);
+    t2 = *((unsigned char *)t3);
+    t5 = (t2 == (unsigned char)3);
+    if (t5 != 0)
+        goto LAB12;
+
+LAB13:    t1 = (t0 + 2312U);
+    t3 = *((char **)t1);
+    t2 = *((unsigned char *)t3);
+    t5 = (t2 == (unsigned char)4);
+    if (t5 != 0)
+        goto LAB14;
+
+LAB15:
+LAB6:    goto LAB3;
+
+LAB5:    xsi_set_current_line(118, ng0);
+    t3 = (t0 + 4688);
+    t7 = (t3 + 56U);
+    t8 = *((char **)t7);
+    t9 = (t8 + 56U);
+    t10 = *((char **)t9);
+    *((unsigned char *)t10) = (unsigned char)2;
+    xsi_driver_first_trans_fast_port(t3);
+    xsi_set_current_line(119, ng0);
+    t1 = (t0 + 4752);
+    t3 = (t1 + 56U);
+    t4 = *((char **)t3);
+    t7 = (t4 + 56U);
+    t8 = *((char **)t7);
+    *((unsigned char *)t8) = (unsigned char)2;
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB6;
+
+LAB8:    xsi_set_current_line(122, ng0);
+    t1 = (t0 + 4688);
+    t4 = (t1 + 56U);
+    t7 = *((char **)t4);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    *((unsigned char *)t9) = (unsigned char)3;
+    xsi_driver_first_trans_fast_port(t1);
+    xsi_set_current_line(123, ng0);
+    t1 = (t0 + 4752);
+    t3 = (t1 + 56U);
+    t4 = *((char **)t3);
+    t7 = (t4 + 56U);
+    t8 = *((char **)t7);
+    *((unsigned char *)t8) = (unsigned char)2;
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB6;
+
+LAB10:    xsi_set_current_line(126, ng0);
+    t1 = (t0 + 4688);
+    t4 = (t1 + 56U);
+    t7 = *((char **)t4);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    *((unsigned char *)t9) = (unsigned char)2;
+    xsi_driver_first_trans_fast_port(t1);
+    xsi_set_current_line(127, ng0);
+    t1 = (t0 + 4752);
+    t3 = (t1 + 56U);
+    t4 = *((char **)t3);
+    t7 = (t4 + 56U);
+    t8 = *((char **)t7);
+    *((unsigned char *)t8) = (unsigned char)2;
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB6;
+
+LAB12:    xsi_set_current_line(130, ng0);
+    t1 = (t0 + 4688);
+    t4 = (t1 + 56U);
+    t7 = *((char **)t4);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    *((unsigned char *)t9) = (unsigned char)2;
+    xsi_driver_first_trans_fast_port(t1);
+    xsi_set_current_line(131, ng0);
+    t1 = (t0 + 4752);
+    t3 = (t1 + 56U);
+    t4 = *((char **)t3);
+    t7 = (t4 + 56U);
+    t8 = *((char **)t7);
+    *((unsigned char *)t8) = (unsigned char)2;
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB6;
+
+LAB14:    xsi_set_current_line(134, ng0);
+    t1 = (t0 + 4688);
+    t4 = (t1 + 56U);
+    t7 = *((char **)t4);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    *((unsigned char *)t9) = (unsigned char)2;
+    xsi_driver_first_trans_fast_port(t1);
+    xsi_set_current_line(135, ng0);
+    t1 = (t0 + 4752);
+    t3 = (t1 + 56U);
+    t4 = *((char **)t3);
+    t7 = (t4 + 56U);
+    t8 = *((char **)t7);
+    *((unsigned char *)t8) = (unsigned char)3;
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB6;
+
+}
+
+
+extern void work_a_0478438494_3212880686_init()
+{
+	static char *pe[] = {(void *)work_a_0478438494_3212880686_p_0,(void *)work_a_0478438494_3212880686_p_1,(void *)work_a_0478438494_3212880686_p_2};
+	xsi_register_didat("work_a_0478438494_3212880686", "isim/Test_MaquinaExpendedora_isim_beh.exe.sim/work/a_0478438494_3212880686.didat");
+	xsi_register_executes(pe);
+}
